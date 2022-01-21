9ae18442a5aaf79b12c4778453d662848c17bd48
Tim Batty <tbatty7@gmail.com>
2022-01-20 19:00:21 -0500

    allows rerouting to a different url and path

---
+++ src/main/java/com/battybuilds/springcloudgateway/SpringcloudgatewayApplication.java
@@ -0,0 +1,13 @@
+package com.battybuilds.springcloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudgatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/battybuilds/springcloudgateway/SpringcloudgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.battybuilds.springcloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
